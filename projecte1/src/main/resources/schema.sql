13a0d38e05808e036e757dda03a049f9367276e7
Mikleiper <mimuba55@gmail.com>
2026-02-05 11:39:57 +0100

    controller crear imatge

---
+++ projecte1/src/main/resources/schema.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS exercicis;
+CREATE TABLE user (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nivell INT NOT NULL,
+    tipus VARCHAR(100) NOT NULL,
+    durada INT NOT NULL,
+    material VARCHAR(100),
+    imagen VARCHAR(500) NULL,
+    ultimAcces timestamp NULL DEFAULT NULL,
+    dataCreated timestamp NULL DEFAULT NULL,
+    dataUpdated timestamp NULL DEFAULT NULL
+);
